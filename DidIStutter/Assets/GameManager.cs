a82adb4e15c4bdc80350b299b5873c9c282a982a
Josh textor <joshtk1994@hotmail.com>
2016-10-05 00:56:43 +1000

    added all disalogue for player and eva. new fonts

--- DidIStutter/Assets/GameManager.cs
+++ DidIStutter/Assets/GameManager.cs
@@ -34,21 +34,128 @@ public class GameManager : MonoBehaviour
 
         if (Player1Line == 1)
         {
-            Player1Ref = ("Oh yeah? me too. Me too.");
+            Player1Ref = ("I had an amazing time too.");
         }
         if (Player1Line == 2)
         {
-            Player1Ref = ("Yeah I wonder hey?");
+            Player1Ref = ("oh yeah me too. Me too.");
         }
         if (Player1Line == 3)
         {
-            Player1Ref = ("He throws it around like it weighs nothing as well.");
+            Player1Ref = ("the movie was great. lots of action and a little romance.");
         }
         if (Player1Line == 4)
         {
-            Player1Ref = ("Haha, admantm or something?");
+            Player1Ref = ("I Love the swiss chocolate flavour and the chocolate sauce on top.");
         }
-
+        if (Player1Line == 5)
+        {
+            Player1Ref = ("yep, me too.");
+        }
+        if (Player1Line == 6)
+        {
+            Player1Ref = ("it’s cool the way it bounces off everything too.");
+        }
+        if (Player1Line == 7)
+        {
+            Player1Ref = ("yeah I wonder.");
+        }
+        if (Player1Line == 8)
+        {
+            Player1Ref = ("aaaaaadmantm or something?");
+        }
+        if (Player1Line == 9)
+        {
+            Player1Ref = ("the blue of your dress matches your eyes.");
+        }
+        if (Player1Line == 10)
+        {
+            Player1Ref = ("your wavy hair is adoreable");
+        }
+        if (Player1Line == 11)
+        {
+            Player1Ref = ("your soft looking lips");
+        }
+        if (Player1Line == 12)
+        {
+            Player1Ref = ("thanks.");
+        }
+        if (Player1Line == 13)
+        {
+            Player1Ref = ("i’d love to go out again on saturday.");
+        }
+        if (Player1Line == 14)
+        {
+            Player1Ref = ("the party sounds great.");
+        }
+        if (Player1Line == 15)
+        {
+            Player1Ref = ("oh who's your favourite band?");
+        }
+        if (Player1Line == 16)
+        {
+            Player1Ref = ("don't you think it's too soon to meet your whole family?");
+        }
+        if (Player1Line == 17)
+        {
+            Player1Ref = ("arrruhhhhhmm I think i’m busy saturday.");
+        }
+        if (Player1Line == 18)
+        {
+            Player1Ref = ("I feel intoxicated by you.");
+        }
+        if (Player1Line == 19)
+        {
+            Player1Ref = ("I can’t breathe when I’m around you.");
+        }
+        if (Player1Line == 20)
+        {
+            Player1Ref = ("this has been the best night of my life.");
+        }
+        if (Player1Line == 21)
+        {
+            Player1Ref = ("you’re cool.");
+        }
+        if (Player1Line == 22)
+        {
+            Player1Ref = ("I want to love you.");
+        }
+        if (Player1Line == 23)
+        {
+            Player1Ref = ("you brighten my incredibly dim world.");
+        }
+        if (Player1Line == 24)
+        {
+            Player1Ref = ("you bring joy to my world.");
+        }
+        if (Player1Line == 25)
+        {
+            Player1Ref = ("…");
+        }
+        if (Player1Line == 26)
+        {
+            Player1Ref = ("sorry");
+        }
+        if (Player1Line == 27)
+        {
+            Player1Ref = ("yeahhh");
+        }
+        if (Player1Line == 28)
+        {
+            Player1Ref = ("I am kinda weird.");
+        }
+        if (Player1Line == 29)
+        {
+            Player1Ref = ("I just have trouble speaking sometimes.");
+        }
+        if (Player1Line == 30)
+        {
+            Player1Ref = ("I think I really like you.");
+        }
+        if (Player1Line == 31)
+        {
+            Player1Ref = ("of course. I would really like to get to know you better.");
+        }
         UpdateText();
     }
 
